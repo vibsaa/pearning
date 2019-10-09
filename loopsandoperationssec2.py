4832aaddb0190094e6bd57582f348ffc8b719629
vibsaa <rocketvibs@outlook.com>
2019-10-09 14:20:30 +0530

    commit

---
+++ loopsandoperationssec2.py
@@ -0,0 +1,27 @@
+"""is_programmer=20
+if is_programmer is False:
+    print("g")
+else:
+    print("n")"""
+
+"""check=True
+while check:
+    user_in=input()
+    check=user_in=="p"
+    if user_in=="p":
+        print("Hello")
+    elif user_in=="q":
+        check=False"""
+
+"""for i in range(5,10):
+    print(i)"""
+
+for i in range (1,101):
+    if i%3==0:
+        print("Fizz")
+    elif i%5==0:
+        print("Buzz")
+    elif i%3==0 & i%5==0:
+        print("FizzBuzz")
+
+
--- operatorsandexp.py
+++
@@ -1,9 +0,0 @@
-
-
-my_friends = {
-    'Jose': {'last_seen': 6},
-    'Rolf': {'surname': 'Smith'},
-    'Anne': 6
-}
-
-print(my_friends['Jose'])
