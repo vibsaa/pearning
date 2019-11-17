9a6b45e1397741c235292858169bf4b0c9b284bc
vibsaa <rocketvibs@outlook.com>
2019-11-17 18:09:51 +0530

    we are dying

--- gui_dev_cts/pyserial.py
+++ gui_dev_cts/pyserial.py
@@ -16,6 +16,30 @@ def start():
         i=i+1
     data.close()
 
+def convert():
+    csv_data=open('points.txt','r')
+    lines=csv_data.readlines()
+    csv_data.close()
+    #print(lines)
+    lines=[line.strip() for line in lines]
+    #print(lines)
+    csv_op=open('datapoints.txt','w')
+    csv_op.close()
+
+    for line in lines:
+        data_sep=line.split(',')
+        vdadc=int(data_sep[0])
+        idadc=int(data_sep[1])
+        #print(f" {vdadc} , {idadc} ")
+        voltage=vdadc*.00654
+        current=(idadc*.00654)/6.67
+        #print(f"the v value is {voltage:.3f} and i value is {current:.3f} ")
+        csv_val=','.join([str(voltage),str(current)])
+        csv_op=open('datapoints.txt','a')
+        #print(csv_val)
+        csv_op.write(f"{csv_val}\n")
+        csv_op.close()
+    
 
 root=tk.Tk()
 root.title("WELCOME TO CURVE TRACER FOR DIODES")
@@ -23,6 +47,6 @@ start_button=ttk.Button(root,text="start",command=start)
 start_button.pack(side='left', fill='x',expand=True)
 quit_button=ttk.Button(root,text='kill',command=root.destroy)
 quit_button.pack(side='left', fill='x',expand=True)
-#convert_button=ttk.Button(root,text="start",command=start)
-#convert_button.pack(side='left', fill='x',expand=True)
+convert_button=ttk.Button(root,text="start",command=convert)
+convert_button.pack(side='left', fill='x',expand=True)
 root.mainloop()
