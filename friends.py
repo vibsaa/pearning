3628c13359909ad155112281e7451ee2419d42ca
vibsaa <rocketvibs@outlook.com>
2019-11-17 16:43:28 +0530

    approaching the end

---
+++ friends.py
@@ -0,0 +1,4 @@
+while(1):
+    x=int(input("no.?"))
+    y=(x*.00656)/6.67
+    print(y)
--- gui_dev_cts/pyserial.py
+++ gui_dev_cts/pyserial.py
@@ -1,8 +1,25 @@
 import serial
-ser=serial.Serial('COM9', baudrate=9600, timeout=1)
+import time
+import tkinter as tk
+from tkinter import ttk 
 
+#ser=serial.Serial('COM9', baudrate=9600, timeout=1)
+#i=0
+def start():
+    ser=serial.Serial('COM9', baudrate=9600, timeout=1)
+    i=0
+    ser.write(b's')
+    data=open('points.txt','w')
+    while(i<68):
+        avrdata=ser.readline().decode('ascii')
+        data.write(avrdata)
+        i=i+1
+    data.close()
 
-ser.write(b's')
-while(1):
-    avrdata=ser.readline().decode('ascii')
-    print(avrdata)
+root=tk.Tk()
+root.title("WELCOME TO CURVE TRACER FOR DIODES")
+start_button=ttk.Button(root,text="start",command=start)
+start_button.pack(side='left', fill='x',expand=True)
+quit_button=ttk.Button(root,text='kill',command=root.destroy)
+quit_button.pack(side='left', fill='x',expand=True)
+root.mainloop()
