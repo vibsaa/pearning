3d9cb2a5b4d8e74c217d664b3170647858e154c0
vibsaa <rocketvibs@outlook.com>
2019-12-13 13:25:24 +0530

    fin

--- gui_dev_cts/cts_pyserial_ver2.py
+++ gui_dev_cts/cts_pyserial_ver2.py
@@ -15,7 +15,7 @@ import mplcursors
 #ser=serial.Serial('COM9', baudrate=9600, timeout=1)
 #i=0
 def start():
-    ser=serial.Serial('COM9', baudrate=9600, timeout=1)
+    ser=serial.Serial('COM3', baudrate=9600, timeout=1)
     i=0
     ser.write(b's')
     data1=open('points.txt','w')
